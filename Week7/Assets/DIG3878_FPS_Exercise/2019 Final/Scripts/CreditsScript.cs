5f5e37c510381786a3c5fe5855b2eb12b5ffc59a
rmm72 <rmm72@users.noreply.github.com>
2019-04-22 01:19:12 -0400

    Tweaks
    Getting ready for Open House

---
+++ Week7/Assets/DIG3878_FPS_Exercise/2019 Final/Scripts/CreditsScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScript : MonoBehaviour {
+
+    public GameObject quitButton;
+
+	// Use this for initialization
+	void Start () {
+        quitButton.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	public void OnTriggerEnter2D (Collision col) {
+		if (col.gameObject.tag == "Logo")
+        {
+            quitButton.SetActive(false);
+        }
+	}
+}
